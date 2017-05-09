0c2fd06de45f2533ed209c4dcf38cd5a55bdf5b2
lcola-wing <329527209@qq.com>
2017-05-09 15:36:35 +0800

    add 统考专升本页面

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,23 @@ class HomeController < ApplicationController
   def index
   end
   
+  #网络教育
   def distance_education
     
   end
   
+  #自学考试
   def self_taught_examination
     
   end
   
+  #成人高考
   def adult_entrance
+    
+  end
+  
+  #统考专升本
+  def nationwide_entrance_examination
     
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'ycjy/' => 'home#distance_education'
   get 'zxks/' => 'home#self_taught_examination'
   get 'crgk/' => 'home#adult_entrance'
-   
+  get 'tkzsb/' => 'home#nationwide_entrance_examination'
   
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
