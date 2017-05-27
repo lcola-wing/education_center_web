96f532f3d69656f4292f1b4fb4bbecc52525849e
alien <bill@ACA80B7F.ipt.aol.com>
2017-05-27 17:17:17 +0800

    fix bug

--- app/assets/javascripts/home/main.js
+++ app/assets/javascripts/home/main.js
@@ -1,1 +1,13 @@
-alert("1");
+window.onload = function() {
+
+    $('.bar ul li').mouseover(function() {
+        $(this).parent().children().removeClass("on");
+        $(this).addClass("on");
+        var index = $(this).index();
+        var form = $(this).parent().parent().next().children();
+        form.removeClass("selected");
+        form[index].className += " selected";
+    });
+
+
+}
